30b4a63062fa0601657d82797b737010f2a8bf70
lhnguyenVP21 <lhnguyen21@vp.fitus.edu.vn>
2024-03-21 10:49:44 +0700

    done navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import logo from './../assets/images/logo.png'
+import { HiHome,
+    HiMagnifyingGlass,
+    HiStar,
+    HiPlayCircle,
+    HiTv } from "react-icons/hi2";
+import { HiPlus,HiDotsVertical } from "react-icons/hi";
+import HeaderPages from './HeaderPages';
+function Header() {
+    const [toggle,setToggle]=useState(false);
+    const menu=[
+        {
+            name:'HOME',
+            icon:HiHome
+        },
+        {
+            name:'SEARCH',
+            icon:HiMagnifyingGlass
+        },
+        {
+            name:'WATCH LIST',
+            icon:HiPlus
+        },
+        {
+            name:'ORIGINALS',
+            icon:HiStar
+        },
+        {
+            name:'MOVIES',
+            icon:HiPlayCircle
+        },
+        {
+            name:'SERIES',
+            icon:HiTv
+        }
+    ]
+  return (
+    <div className='flex items-center justify-between p-5'>
+        <div className='flex  gap-8 items-center'>
+            <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
+            <div className='hidden md:flex gap-8'>
+                {menu.map((item)=>(<HeaderPages name={item.name} Icon={item.icon} />))}
+            </div>
+            <div className='flex md:hidden gap-5'>
+                {menu.map((item,index)=>index <3 &&( <HeaderPages name={''} Icon={item.icon} />))}
+                <div className='md:hidden' onClick={()=>setToggle(!toggle)}>       
+                    <HeaderPages name={''} Icon={HiDotsVertical} />
+                        {toggle? 
+                    <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+                        {menu.map((item,index)=>index>2&&(<HeaderPages name={item.name} Icon={item.icon} />))}
+                    </div>:null}
+                </div> 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
